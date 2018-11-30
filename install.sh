0a159ce25266f398851cd66888b68cf0e10b6d62
Ralph Walters <44847118+RalphWalters@users.noreply.github.com>
2018-11-30 14:25:44 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 cd Download
 
 # NodeJS LTS
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 sudo yum install -y nodejs
 
 # Chromium
@@ -23,7 +23,7 @@ sudo yum install -y chromium
 # sudo yum install -y ${URL}
 
 # Microsoft Visual Studio Code
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo yum check-update
 sudo yum install -y code
