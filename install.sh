f7e113c3c9f0e0abf8b7a77e37841fe31ace4d67
Ralph Walters <44847118+RalphWalters@users.noreply.github.com>
2018-12-05 10:44:44 -0600

    Updated install bash script for latest .NET SDK

--- install.sh
+++ install.sh
@@ -32,8 +32,9 @@ sudo yum check-update
 sudo yum install -y code
 
 # Microsoft .NET SDK
+# https://dotnet.microsoft.com/download/linux-package-manager/rhel/sdk-2.2.100
 sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
-sudo yum install -y dotnet-sdk-2.1
+sudo yum install -y dotnet-sdk-2.2
 
 # Miniconda Python
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
